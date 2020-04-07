428cebd06e238d361182dd375b4b958a8a6c0ac7
rorygwozdz <36833142+rorygwozdz@users.noreply.github.com>
2020-04-07 07:03:12 -0700

    daniels_cave created

--- shatterproofing_code/options_wrangler.py
+++ shatterproofing_code/options_wrangler.py
@@ -9,7 +9,8 @@ class OptionsWrangler:
             pass
         else:
             raise TypeError(f"Unsupported data type. Got {type(data)}, expected str or pd.DataFrame.")
-        self.data = data.set_index(['trade_date', 'expirDate', 'strike'])
+        dedeuped_data = data.drop_duplicates(['trade_date', 'expirDate', 'strike'])
+        self.data = dedeuped_data.set_index(['trade_date', 'expirDate', 'strike'])
 
     def get_expo_chain(self, trade_date, expiration=None):
         if expiration:
@@ -30,17 +31,63 @@ class OptionsWrangler:
 
         return raw_chain.reset_index('strike')
 
+    def get_option_value_by_strike(self, strike, side, *, trade_date=None, expo=None, chain=pd.DataFrame()):
+        # supports getting exactly the option or getting the chain from memory
+        if chain.empty:
+            chain = self.get_expo_chain(trade_date=trade_date, expiration=expo)
+        assert strike in chain['strike'].values, "Strike not available in chain."
+        strike_query = f"strike == {strike}"
+        chain_row = chain.query(strike_query)
+        if side in ["call", "c"]:
+            return chain_row['cValue'].values
+        elif side in ["put", "p"]:
+            return chain_row['pValue'].values
+        else:
+            raise NameError(f"Unsupported side type. Got {side}, expected one of: call, c, put, p.")
 
-#
-#
-# wrangler = OptionsWrangler(data=options_data)
-# # returns the the expiration for an option
-# wrangler.get_expo_chain(expo_chain, trade_date) # throws not valid expo, not in dataset (before, after)
-# # returns the the expiration for an option
-# wrangler.get_option_expo(option)
+    def get_option_value_by_delta(self, delta, side, *, trade_date=None, expiration=None, chain=pd.DataFrame()):
+        # supports getting exactly the option or getting the chain then the option if given a trade_date and expo
+        assert 0 < delta < 1, "Delta not bounded by zero or one."
+        if chain.empty:
+            if not expiration:
+                raise TypeError("Expo can not be None with no chain given.")
+            chain = self.get_expo_chain(trade_date=trade_date, expiration=expiration)
+        if side in ["call", "c"]:
+            chain_row = chain.iloc[(chain['delta'] - delta).abs().argsort()[:1]]
+            return chain_row['cValue'].values
+        elif side in ["put", "p"]:
+            chain_row = chain.iloc[(chain['delta'] - (1 - delta)).abs().argsort()[:1]]
+            return chain_row['pValue'].values
+        else:
+            raise NameError(f"Unsupported side type. Got {side}, expected one of: call, c, put, p.")
+
+    def get_option_history(self, *, start_trade_date, expiration, strike, side):
+        history_query = f"trade_date >= '{start_trade_date}' and expirDate == '{expiration}' and strike == {strike}"
+        relevant_option_history = self.data.query(history_query)
+        if side in ["call", "c"]:
+            options_history = relevant_option_history[['stkPx', 'cValue', 'cOi']]
+            return options_history
+        elif side in ["put", "p"]:
+            options_history = relevant_option_history[['stkPx', 'pValue', 'pOi']]
+            return options_history
+        else:
+            raise NameError(f"Unsupported side type. Got {side}, expected one of: call, c, put, p.")
+
+
+
 # wrangler.get_option_history(strike, expo, dtarting_trade_date) # throws no such option, not in dataset
+
+
 # wrangler.get_days_historical_chain() # throws not in data set
-# wrangler.get_imp_vol(expo, trade_date, strike)
+
+# gets the implied volatility for trade date and month, returns the smile data
+# wrangler.get_imp_vol(expo, trade_date)
+
+
 # wrangler.get_call_value(expo, trade_date, strike) # throws not in dataset
+
+
 # wrangler.get_put_value(expo, trade_date, strike) # throws not in dataset
+
+
 # wrangler.get_market(expo, trade_date, strike) # throws not in dataset
--- shatterproofing_code/test_wrangler.py
+++ shatterproofing_code/test_wrangler.py
@@ -62,26 +62,82 @@ def test_get_expo_chain(uso_wrangler):
 
 
 # gets the value of a call at a particular strike
-def test_get_option_value(uso_wrangler):
+def test_get_option_value_by_strike(uso_wrangler):
 
     trade_date = '2020-02-14'
     expo = '2020-04-17'
     strike = 10
     side = 'call'
-    uso_10_strike_call = uso_wrangler.get_option_value(trade_date, expo, strike, side)
+    uso_10_strike_call = uso_wrangler.get_option_value_by_strike(strike, side, trade_date=trade_date, expo=expo)
     assert 1.22 == uso_10_strike_call
 
     side = 'put'
-    uso_10_strike_put = uso_wrangler.get_option_value(trade_date, expo, strike, side)
+    uso_10_strike_put = uso_wrangler.get_option_value_by_strike(strike, side, trade_date=trade_date, expo=expo)
     assert .25 == uso_10_strike_put
+
+
+    # test ing by chain
+    sample_chain = uso_wrangler.get_expo_chain(trade_date, expo)
+    side = 'put'
+    uso_10_strike_put_by_chain = uso_wrangler.get_option_value_by_strike(strike, side, chain=sample_chain)
+    assert .25 == uso_10_strike_put_by_chain
 
     # throws index error
     strike = 1000
-    with pytest.raises(IndexError):
-        uso_1000_strike_put = uso_wrangler.get_option_value(trade_date, expo, strike, side)
+    with pytest.raises(AssertionError):
+        uso_1000_strike_put = uso_wrangler.get_option_value_by_strike(strike, side, chain=sample_chain)
         assert .25 == uso_1000_strike_put
 
 
+# gets the value of a call at a particular strike
+def test_get_option_value_by_delta(uso_wrangler):
+
+    trade_date = '2020-02-14'
+    expo = '2020-04-17'
+    delta = .35
+    side = 'call'
+    uso_35_delta_call = uso_wrangler.get_option_value_by_delta(delta, side, trade_date=trade_date, expo=expo)
+    assert 0.36 == uso_35_delta_call
+
+    side = 'put'
+    uso_35_delta_put = uso_wrangler.get_option_value_by_delta(delta, side, trade_date=trade_date, expo=expo)
+    assert 0.39 == uso_35_delta_put
+
+    # test ing by chain
+    sample_chain = uso_wrangler.get_expo_chain(trade_date, expo)
+    side = 'put'
+    uso_35_delta_put_by_chain = uso_wrangler.get_option_value_by_delta(delta, side, chain=sample_chain)
+    assert 0.39 == uso_35_delta_put_by_chain
+
+    # throws index error
+    delta = 1000
+    with pytest.raises(AssertionError):
+        uso_1000_delta_put = uso_wrangler.get_option_value_by_delta(delta, side, chain=sample_chain)
+        assert .39 == uso_1000_delta_put
+
+    # test no chain no expo
+    delta = .5
+    with pytest.raises(TypeError):
+        uso_erro_on_no_expo = uso_wrangler.get_option_value_by_delta(delta, side, trade_date=trade_date)
+        assert .25 == uso_erro_on_no_expo
+
+
+# gets the value of a call at a particular strike
+def test_get_option_history(uso_wrangler):
+    trade_date = '2020-02-14'
+    expo = '2020-04-17'
+    strike = 10
+    side = 'call'
+    uso_10_strike_call_history = uso_wrangler.get_option_history(strike=strike, side=side,
+                                                                 start_trade_date=trade_date, expiration=expo)
+
+    assert len(uso_10_strike_call_history) == 31
+
+    side = 'put'
+    uso_10_strike_put_history = uso_wrangler.get_option_history(strike=strike, side=side,
+                                                                start_trade_date=trade_date, expiration=expo)
+
+    assert len(uso_10_strike_put_history) == len(uso_10_strike_call_history)
 
 
 
